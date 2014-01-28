95cbaad3a3574423feb460ec182572424f9c9682
Liam Stanley <me@liamstanley.net>
2014-01-28 17:18:54 -0500

    URL support, over host support

--- check.php
+++ check.php
@@ -4,15 +4,20 @@
                       array('liamstanley.net', 80, 'Liams Site'),
                       array('google.com', 80, 'Google'),
                       array('livestrap.tk', 80, 'Livestrap'),
-                      array('example.down.site.com', 8080, 'Example')
+                      array('example.down.site.com', 8080, 'Example'),
+                      array('http://cajs.co.uk/', None, 'URL')
                       );
 
 
     // Function that actually pings the port, to see if it's up.
     function check($ip,$port,$name) {
-        $checkSock = @fsockopen($ip, $port, $errno, $errstr, 3);
-        if ($checkSock !== FALSE)
-        {
+        $is_a_url = is_url($ip);
+        if ($is_a_url == True) {
+            $output = url($ip);
+        } else {
+            $output = sock($ip,$port);
+        }
+        if ($output == True) {
             // online!
             $status = "up";
             $nice = "Online";
@@ -27,6 +32,24 @@
                     <h2 class="status">'.$nice.'</h2>
                 </div>
             </div>';
+    }
+
+    function sock($ip,$port) {
+        $checkSock = @fsockopen($ip, $port, $errno, $errstr, 3);
+        if ($checkSock !== FALSE) return True;
+        return False;
+    }
+
+    function url($site) {
+        $headers = @get_headers($site);
+        if(strpos($headers[0],'200')===false) return False;
+        return True;
+        
+    }
+
+    function is_url($item) {
+        if (strpos($item,'/') !== false) return True;
+        return False;
     }
 
     // Let's call it. :)
